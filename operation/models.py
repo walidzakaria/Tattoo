94434b94c394a19c592c1da00b1b56850095005f
Walid Zakaria <walidpiano@yahoo.com>
2025-03-05 16:45:30 +0200

    add operation app

--- operation/models.py
+++ operation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Statistics(models.Model):
+    info = models.CharField(max_length=100)
+    value = models.IntegerField()
+    logo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.info
+
+    class Meta:
+        verbose_name = 'Statistics'
+        verbose_name_plural = 'Statistics'
---
+++ operation/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Statistics
+
+
+class StatisticsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Statistics
+        fields = '__all__'
